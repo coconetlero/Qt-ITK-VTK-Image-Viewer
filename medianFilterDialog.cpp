6d660339b7049b70eb5d2ccd4ca0fcd360ffb661
zian <coconetlero@gmail.com>
2011-09-20 13:41:20 -0500

    create meidian filter dialog

---
+++ medianFilterDialog.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   medianFilterDialog.cpp
+ * Author: zian
+ * 
+ * Created on 14 de septiembre de 2011, 16:52
+ */
+#include <QtGui>
+#include <QDialog>
+
+#include "medianFilterDialog.h"
+
+MedianFilterDialog::MedianFilterDialog(QWidget *parent) : QDialog(parent) {
+    this->setupUi(this);
+    
+}
+
+MedianFilterDialog::~MedianFilterDialog() {
+}
+
---
+++ medianFilterDialog.h
@@ -0,0 +1,26 @@
+/* 
+ * File:   medianFilterDialog.h
+ * Author: zian
+ *
+ * Created on 14 de septiembre de 2011, 16:52
+ */
+
+#ifndef MEDIANFILTERDIALOG_H
+#define	MEDIANFILTERDIALOG_H
+
+#include <QDialog>
+
+#include "ui_MedianFilterDialog.h"
+
+class MedianFilterDialog : public QDialog, public Ui::MedianFilterDialog {
+
+public:
+    MedianFilterDialog(QWidget *parent = 0);
+
+    virtual ~MedianFilterDialog();
+private:
+
+};
+
+#endif	/* MEDIANFILTERDIALOG_H */
+
