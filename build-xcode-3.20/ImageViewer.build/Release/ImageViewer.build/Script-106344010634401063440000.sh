22e730157ff63e923a8618d3aba14f2b0f5ff7ac
Zian Fanti <zian@Mac-Pro-Imagenes.local>
2011-11-28 14:32:05 -0600

    remove some files

--- build-xcode-3.20/ImageViewer.build/Release/ImageViewer.build/Script-106344010634401063440000.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/zian/Documents/Proyectos/Qt-ITK-VTK-Image-Viewer/build-xcode-3.20 -f /Users/zian/Documents/Proyectos/Qt-ITK-VTK-Image-Viewer/build-xcode-3.20/CMakeScripts/ImageViewer_cmakeRulesBuildPhase.make$CONFIGURATION all
--- build-xcode/ImageViewer.build/Debug/ImageViewer.build/Script-146703014670301467030000.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/zian/Documents/Proyectos/Qt-ITK-VTK-Image-Viewer/build-xcode -f /Users/zian/Documents/Proyectos/Qt-ITK-VTK-Image-Viewer/build-xcode/CMakeScripts/ImageViewer_postBuildPhase.make$CONFIGURATION all
