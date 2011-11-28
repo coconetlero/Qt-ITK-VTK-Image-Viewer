81eb4520f8c302d22753d81221d80d7b0da04442
Zian Fanti <zian@Mac-Pro-Imagenes.local>
2011-11-28 13:52:30 -0600

    Fix Warning: Relevant clases found. In file MedianFilterDialog.h

---
+++ build-xcode-3.20/ImageViewer.build/Release/ImageViewer.build/Script-106288010628801062880000.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make -C /Users/zian/Documents/Proyectos/Qt-ITK-VTK-Image-Viewer/build-xcode-3.20 -f /Users/zian/Documents/Proyectos/Qt-ITK-VTK-Image-Viewer/build-xcode-3.20/CMakeScripts/ReRunCMake.make
